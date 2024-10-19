3449e7124b2eba95b94aef374d8b12f4ee522009
Eduardo Peruzzo <eduardo.peruzzo05@gmail.com>
2024-10-19 12:18:10 -0300

    divisão das áreas do detalhamento de projeto

--- src/components/DetalhesProjeto/Style.js
+++ src/components/DetalhesProjeto/Style.js
@@ -1,12 +1,30 @@
 import styled from "styled-components";
 
 const AreaInicial = styled.div`
-    width: 100vw;
-    height: auto;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 10px;
+    width: 100%;
+    height: 25em;
 `
 
 const ImagemProjeto = styled.div`
-
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50%;
 `
 
-export {AreaInicial, ImagemProjeto}
+const InfoProjeto = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+`
+
+const DescProjeto = styled.div`
+    width: 100%;
+`
+
+export {AreaInicial, ImagemProjeto, InfoProjeto, DescProjeto}
