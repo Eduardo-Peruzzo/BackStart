12ae54a74d7bc60525ee5fff4c40a225050c22f2
Eduardo Peruzzo <eduardo.peruzzo05@gmail.com>
2024-10-31 16:45:06 -0300

    Update Fotos.jsx

--- src do professor/pages/Fotos.jsx
+++ src do professor/pages/Fotos.jsx
@@ -4,11 +4,19 @@ import dados from "../data/computadores.json";
 import Protegida from "./Protegida";
 
 const Fotos = () => {
+
+  const {id} = useParams();
+
+  const dadosFiltrados = dados.filter(
+    (elemento) => (elemento.id === parseInt(id) || !id)
+    
+  )
+
  
   return (
       <Protegida>
         <ListContainer>
-          {dados.map(
+          {dadosFiltrados.map(
             (el, index) => (
               <FotoCard 
                 key={index}
