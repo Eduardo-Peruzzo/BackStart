3f1fcfc46516b1f45976aaf9613b1c148f9c91dd
MateusSachinho <herculesxaquires@gmail.com>
2024-11-07 14:04:47 -0300

    mudanças dos números, exemplos paginacao

--- src/components/SNos/SNos.jsx
+++ src/components/SNos/SNos.jsx
@@ -11,7 +11,7 @@ const SNos = () => (
                 <p>Contato:</p>
                 <ul>
                     <li>eduardo.peruzzo05@gmail.com</li>
-                    <li>Telefone: +55 (21) 99206-8454</li>
+                    <li>Telefone: +55 (21) 99999-9999</li>
                     <li><a href="https://github.com/Eduardo-Peruzzo">Github</a></li>
                 </ul>
             </Ficha>
@@ -22,7 +22,7 @@ const SNos = () => (
                 <p>Contato:</p>
                 <ul>
                     <li>pedroselecto@gmail.com</li>
-                    <li>Telefone: +55 (21) 99998-1199</li>
+                    <li>Telefone: +55 (21) 99999-9999</li>
                     <li><a href="https://github.com/Pedroselecto">Github</a></li>
                 </ul>
             </Ficha>
@@ -33,18 +33,18 @@ const SNos = () => (
                 <p>Contato:</p>
                 <ul>
                     <li>herculesxaquires@gmail.com</li>
-                    <li>Telefone: +55 (21) 97998-2231</li>
+                    <li>Telefone: +55 (21) 99999-9999</li>
                     <li><a href="https://github.com/MateusSachinho">Github</a></li>
                 </ul>
             </Ficha>
             <Ficha>
                 <img src="imagens/sobre-nos/estevao.png"/>
                 <h3>Estevão Batista de Moraes</h3>
-                <p> Tenho 7 anos, cursando segundo período do curso de Ciência de Dados do IBMEC.</p>
+                <p> Tenho 17 anos, cursando segundo período do curso de Ciência de Dados do IBMEC.</p>
                 <p>Contato:</p>
                 <ul>
                     <li>estevaomoraes48@gmail.com</li>
-                    <li>Telefone: +55 (21) 96568-1220</li>
+                    <li>Telefone: +55 (21) 99999-9999</li>
                     <li><a href="https://github.com/EstevaoMO">Github</a></li>
                 </ul>
             </Ficha>
